100371362ea2c179cfa9d3d7779fd401df9a15e9
subrat <subratj611@gmail.com>
2022-12-12 11:08:25 +0530

    aboutus and commingsoon page added

---
+++ pages/commingsoon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav-bar />
+    <div class="container">
+      <h1>comming soon</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommingSoon',
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+
+  h1 {
+    font-family: 'adobo';
+    text-transform: uppercase;
+    color: #fff;
+    font-size: 3rem;
+    text-align: center;
+    letter-spacing: 0.2em;
+  }
+}
+</style>
