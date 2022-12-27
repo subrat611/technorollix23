eea06562da50b4a8fea0d744ba4e45d0b4a2ebaa
subrat <subratj611@gmail.com>
2022-12-27 20:33:25 +0530

    about  page updated

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -13,7 +13,7 @@
           class="opju-bg-image"
         />
         <div class="overlay"></div>
-        <p class="title">about</p>
+        <p class="title">about us</p>
       </div>
 
       <!-- about -->
@@ -22,19 +22,18 @@
         <div class="about-opju">
           <p class="about-title">about o.p. jindal university</p>
           <p class="about-para">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam rerum
-            error non natus debitis hic ipsum esse, doloremque dolorum optio
-            labore ipsam similique aspernatur, placeat repellendus earum
-            excepturi eveniet ratione soluta ducimus eius libero. Possimus
-            aliquid voluptatem vel, praesentium fugit ducimus, quae maiores
-            quibusdam sit, quas placeat tempore earum facilis voluptatibus? Fuga
-            voluptatem officia autem aperiam, impedit, exercitationem
-            consequatur, dignissimos tempora mollitia qui commodi! Veritatis
-            ratione dignissimos voluptatem reprehenderit ad odio ducimus
-            quisquam nihil minus magni eum omnis libero rem, qui sit repudiandae
-            optio possimus, cupiditate accusantium consequatur doloribus
-            nesciunt? Rem quasi dignissimos quaerat distinctio ab impedit culpa
-            error sequi?
+            Founded by the Jindal Educaticdon and Welfare Society, OP Jindal
+            University (OPJU) was set up to bring high quality education to its
+            students based on a world class curriculum, the latest teaching
+            methodology and committed faculty members. The multidisciplinary
+            university aims to develop young professionals and future leaders
+            who will not only power growth and development in the state, but
+            also make a mark globally.At the core of the university's philosophy
+            and approach lies the belief that students learn best when exposed
+            to real world situations and when nurtured through enriching
+            interactions with practitioners and professors. We at OPJU believe
+            that every student has innate potential that can be unlocked through
+            quality teaching and mentorship.
           </p>
         </div>
 
@@ -44,33 +43,41 @@
           <p class="patron-title-two">
             SHRI NAVEEN JINDAL Chairman, Jindal Steel and Power Ltd
           </p>
-          <p class="patron-para">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
-            quasi voluptatibus placeat iste atque culpa, perspiciatis molestiae
-            asperiores velit iusto aspernatur dignissimos cupiditate ipsam quod
-            enim quia deserunt officia porro. Nostrum amet ratione quis sit
-            nesciunt vitae reprehenderit, doloribus hic fugiat autem rerum
-            accusamus distinctio iure ipsam nobis qui natus expedita! Explicabo
-            maiores iusto culpa voluptatem aliquid fuga excepturi blanditiis
-            sequi asperiores, animi ex quod distinctio aut error unde placeat in
-            fugiat numquam illo nemo hic nihil modi. Quia ad enim molestias
-            dolores maiores eaque earum ducimus fuga laborum voluptatum,
-            asperiores labore, totam ipsam nulla modi nisi, consectetur
-            consequuntur sequi.
+          <p class="patron-para-one">
+            When OPJU was established 8 years ago in Punjipathra, Raigarh
+            District it was the culmination of the dream of my father and the
+            founding Chairman of OP Jindal Group, Shri O. P. Jindal. JSPL’s
+            humble beginnings are rooted in the District of Raigarh, where it
+            began as a small steel making operation in the early 1990’s and
+            through the hard work and dedication of the founders and employees
+            today boasts of a 3.6 MTPA steel plant, a 3400 MW Power plant in
+            Tamnar – the first independent power producer, Nalwa Steel Works,
+            Structural Steel Division, and OP Jindal Industrial Park,
+            Punjipathra. For a community that means so much to the success of
+            JSPL, it was our dream to start a University in the area as both a
+            way to give back to the region, but more so, a deeply rooted
+            commitment to educate the youth of the area, as only an educated
+            youth can help the nation develop to its full potential.
           </p>
-          <p class="patron-para">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Consequatur laborum rem sed suscipit corrupti odio quae
-            exercitationem, dicta assumenda corporis incidunt ea vitae rerum
-            eveniet asperiores impedit deleniti voluptates. Quos nihil sint
-            inventore autem voluptates minus, consectetur optio quo eaque. Eum
-            iure consequuntur, totam perspiciatis distinctio inventore provident
-            quasi quo in ea perferendis saepe consequatur officia, numquam rerum
-            doloremque itaque voluptatem eveniet vero voluptatum est suscipit,
-            iste nostrum omnis. Iure beatae ipsum explicabo laudantium autem
-            cumque quis ad similique sunt iste vitae incidunt labore fuga
-            repellendus modi, quam molestiae omnis. Neque dignissimos blanditiis
-            itaque assumenda quia earum nulla laboriosam recusandae.
+          <p class="patron-para-two">
+            Started in 2008 as an engineering college, today I can proudly say
+            that in a short span of only four years, OPJU has become a role
+            model for institutions of higher education not only in Chhattisgarh,
+            but in the entire nation of what can be achieved with the right
+            motivations, vision, and programs focused on student outcomes,
+            creating vast employment opportunities for the youth of our nation
+            and guiding them along a path to becoming successful contributors to
+            society. With OP Jindal Group’s deep commitment to nation building
+            and focus on core industries of Steel, Mining, Power, and
+            Construction, it is only befitting that OPJU’s vision of becoming
+            the nation’s first private and only university to enhance the core
+            sector capabilities is our commitment to the growth of the nation
+            and realizing our collective dream of building the nation we can all
+            be proud of. As the Patron of an institution of higher learning, I
+            am humbled by the enormous faith the community has bestowed upon me
+            and accept the challenge and the responsibility to build a socially
+            responsible, equitable, and accessible institute we can all take
+            pride in.
           </p>
         </div>
 
@@ -78,18 +85,26 @@
         <div class="about-techno">
           <p class="techno-title">about technorollix</p>
           <p class="techno-para">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi magni
-            aut cum at corrupti consequuntur molestiae facere laborum ullam
-            labore amet, deserunt tenetur officia. Illo repellat ducimus
-            veritatis? Minus officia esse repellat quas molestiae natus optio,
-            eveniet magnam explicabo labore aperiam corporis est magni vitae
-            architecto temporibus quis. Aut voluptates repellendus ut minima
-            vero enim libero laboriosam velit possimus laudantium natus
-            explicabo quam neque laborum quidem, sapiente ab illum corrupti sit
-            ratione quas voluptatem corporis numquam. At labore culpa debitis
-            porro, dolor assumenda facere doloribus id quisquam hic maiores
-            perspiciatis, beatae dolorem tempore sint totam praesentium
-            reiciendis perferendis accusantium aspernatur.
+            Welcome to Central India's Biggest Technial And Cultural Fest
+            <span class="techno">#Technorollix.</span>This event is organized
+            yearly by our students and faculty staff. This event is called the
+            Biggest Technical and Cultural because of its variety in every
+            possible way possible. This fest includes each and every competition
+            and event you can think off from art to gaming, from talks to
+            exhibitions, each and every event has its own charm. Technorollix,
+            the annual techno-Management festival of OP Jindal University, hosts
+            a galaxy of events aimed at boosting the technological and cultural
+            skills inherent in today’s youth, giving them an unrivalled
+            opportunity to showcase their thoughts and ideas. Over the last four
+            years, it has been a platform that has successfully brought
+            students, academicians, and industrial giants under one roof.
+            Technorollix’20, Spanning from 18th to 21st February 2022, aims at
+            giving a spine-tingling ride to inspiring minds and bringing
+            together dynamic, innovative and enterprising students of various
+            colleges (students of B Tech., B.Com, B.Sc., BBA) from all over
+            India on a common platform to participate in different competitive
+            events along with OPJU students, dedicated to experiencing the fun
+            in technology, Management and science.
           </p>
         </div>
       </div>
@@ -108,7 +123,6 @@ export default {
   background: #000;
 
   .about-container {
-
     color: #fff;
     .image-container {
       position: relative;
@@ -129,7 +143,7 @@ export default {
         height: 100%;
         width: 100%;
         background-color: #000;
-        opacity: 0.5;
+        opacity: 0.6;
       }
 
       .title {
@@ -137,17 +151,19 @@ export default {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        font-size: 2rem;
+        font-size: 4rem;
+        font-family: 'Montserrat', sans-serif;
+        letter-spacing: 0.15em;
         text-transform: uppercase;
-        color: #fff;
+        color: #e9ecef;
         font-weight: bold;
       }
     }
 
     .about-content {
-      padding: 1rem 5rem;
+      margin-top: 1rem;
+      padding: 2rem 5rem;
       font-family: 'Poppins', sans-serif;
-      font-weight: 600;
 
       .about-opju {
         margin-bottom: 2rem;
@@ -155,18 +171,42 @@ export default {
           font-size: 2rem;
           text-transform: capitalize;
           margin-bottom: 1.5rem;
+          font-weight: 600;
+        }
+
+        .about-para {
+          letter-spacing: 0.02em;
+          font-family: 'Montserrat', sans-serif;
+          color: #dee2e6;
         }
       }
 
       .patron-container {
         margin-bottom: 2rem;
+
         .patron-title-one {
           font-size: 2rem;
           text-transform: capitalize;
           margin-bottom: 1rem;
+          font-weight: 600;
         }
+
         .patron-title-two {
           font-size: 1.5rem;
+          padding: 0.5rem 0;
+        }
+
+        .patron-para-one {
+          letter-spacing: 0.02em;
+          color: #dee2e6;
+          font-family: 'Montserrat', sans-serif;
+        }
+
+        .patron-para-two {
+          font-family: 'Montserrat', sans-serif;
+          letter-spacing: 0.02em;
+          margin-top: 1.2rem;
+          color: #dee2e6;
         }
       }
 
@@ -176,7 +216,35 @@ export default {
           font-size: 2rem;
           text-transform: capitalize;
           margin-bottom: 1rem;
+          font-weight: 600;
         }
+        .techno-para {
+          color: #dee2e6;
+          font-family: 'Montserrat', sans-serif;
+          letter-spacing: 0.02em;
+
+          .techno {
+            color: orange;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media all and (max-width: '912px') {
+  .about-page {
+    .about-container {
+      .image-container {
+        height: 250px;
+
+        .title {
+          font-size: 1.5rem;
+        }
+      }
+
+      .about-content {
+        padding: 2rem;
       }
     }
   }
