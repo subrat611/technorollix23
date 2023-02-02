778703b1ec12d2e45ef68a644edc8ed547340b71
subrat <subratj611@gmail.com>
2023-02-02 18:57:44 +0530

    aos installation & setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,12 @@ export default {
   css: ['~/assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '~/plugins/aos.js',
+      mode: 'client',
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
